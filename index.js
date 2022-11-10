33cc5e90777a1d016ba243c4c0751220d7461867
Cristian Omar Gonzalez Madrigales <cogonzalezm@zapopan.gob.mx>
2022-11-10 17:32:41 -0600

    Probando webhook

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const app = express()
  
 const {WebhookClient} = require('dialogflow-fulfillment');
 const {Card, Suggestion} = require('dialogflow-fulfillment');
+const mysql = require('mysql')
+
+const conexion = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+      
+  password: '',
+  database: 'ficha_servicio'
+});
 
 const PORT = process.env.PORT ||  5000
 app.get('/', function (req, res) {
@@ -17,22 +26,34 @@ app.post('/webhook', express.json(), function (req, res) {
   // console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
   // console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
  
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
+  function ProbandoWebhook(agent) {
+    const sql = 'SELECT nombre FROM usuarios where ID = 4'
+    conexion.query(sql, (err, results) =>{
+        if(err) throw err;
+        if(results.length > 0){
+            res.json(results);
+            agent.add(`Agregue usuario`, results);
+        }  else {
+            res.send("No hay resultados")
+        }
+
+    });
+   
   }
  
   function fallback(agent) {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
   }
-  function ProbandoWebhook(agent) {
-    agent.add(`Estoy enviando desde el webHook`);
-  }
+  // function ProbandoWebhook(agent) {
+  //   agent.add(`Estoy enviando desde el webHook`);
+  // }
 
   
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('ProbandoWebhook', ProbandoWebhook);
   intentMap.set('ProbandoWebhook', ProbandoWebhook);
   // intentMap.set('your intent name here', yourFunctionHandler);
   // intentMap.set('your intent name here', googleAssistantHandler);
